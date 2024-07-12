37017f4bf390cecf750891e38a594e6f6419f095
eze <linezekdlr@hotmail.com>
2024-07-12 11:33:48 +0200

    feat: dbclient module

---
+++ dbClient.js
@@ -0,0 +1,30 @@
+import { MongoClient } from "mongodb";
+import uri from "./uri.js";
+
+const client = new MongoClient(uri);
+
+async function connect() {
+    try {
+        await client.connect();
+        console.log('Connected successfully to database');
+        return client;
+    } catch (error) {
+        console.error('Connection to database failed:', error);
+        throw error;
+    }
+}
+
+async function closeConnection() {
+    try {
+        await client.close();
+        console.log('Connection closed');
+    } catch (error) {
+        console.error('Error closing connection:', error);
+        throw error;
+    }
+}
+
+export default {
+    connect,
+    closeConnection
+};
