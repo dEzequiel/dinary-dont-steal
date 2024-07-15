9df29b1af219888e19e0290d401741e531e419bc
eze <linezekdlr@hotmail.com>
2024-07-15 13:55:03 +0200

    feat: srp download photo or logo functios

--- utils/resourceFetchers.js
+++ utils/resourceFetchers.js
@@ -83,6 +83,7 @@ function downloadImagesFromDMC(dmcs) {
     })
 }
 
+
 function createFolder(folderName) {
     if(!fs.existsSync(folderName)) {
         fs.mkdirSync(folderName, { recursive: true });
@@ -97,5 +98,40 @@ function createFolderForEntity(path, entity) {
     }
     createFolder(folderPath);
 }
+
+//#region 
+
+function downloadImagesPhoto(images) {
+    if(images.photo.startsWith('/img') || images.photo.startsWith('img')) {
+        const yourttooDomain = 'http://www.yourttoo.com/'
+        http.get(normalize(`${yourttooDomain}${images.photo}`), (res) => {
+            console.log('Downloading from yourttoo...')
+            res.pipe(file)
+        })
+    } else {
+        http.get(normalize(images.photo), (res) => {
+            console.log('Downloading from cloudinary...')
+            res.pipe(file)
+        })
+    }
+}
+
+
+function downloadImagesLogo(images) {
+    if(images.logo.startsWith('/img') || images.logo.startsWith('img')) {
+        const yourttooDomain = 'http://www.yourttoo.com/'
+        http.get(normalize(`${yourttooDomain}${images.logo}`), (res) => {
+            console.log('Downloading from yourttoo...')
+            res.pipe(file)
+        })
+    } else {
+        http.get(normalize(images.logo), (res) => {
+            console.log('Downloading from cloudinary...')
+            res.pipe(file)
+        })
+    }
+}
+
+//#endregion
 
 export { downloadImagesFromDMCProducts, downloadImagesFromDMC }
