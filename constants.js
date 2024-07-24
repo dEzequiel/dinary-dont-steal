492496145cbac6abd57596017a652e27e795d3d0
eze <linezekdlr@hotmail.com>
2024-07-24 13:22:26 +0200

    new val

--- constants.js
+++ constants.js
@@ -3,7 +3,8 @@ const defaultImagesUrl = [
     'http://res.cloudinary.com/open-market-travel/image/upload/v1412587999/avatar.jpg',
     'http://res.cloudinary.com/open-market-travel/image/upload/v1426853495/assets/avatar.jpg',
     'http://res.cloudinary.com/open-market-travel/image/upload/v1426854292/assets/omtempty.png',
-    'https://res.cloudinary.com/open-market-travel/image/upload/v1426854292/assets/omtempty.png'
+    'https://res.cloudinary.com/open-market-travel/image/upload/v1426854292/assets/omtempty.png',
+    'https://graph.facebook.com/100007543428129/picture?type=large',
 ]
 
 const commonCharsOnImages = /[\/\\|*]/g;
