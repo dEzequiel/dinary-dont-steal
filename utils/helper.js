e9014837c56fd694f57057b1ea935c33f24b481b
eze <linezekdlr@hotmail.com>
2024-07-24 12:43:04 +0200

    utils

--- utils/helper.js
+++ utils/helper.js
@@ -1,3 +1,6 @@
+import { defaultImagesUrl } from "../constants.js";
+
+
 export const getImageNameFromURL = (url) => {
     return url.split('/').pop().split('.')[0];
 }
@@ -7,4 +10,17 @@ export const getUrlProtocol = (url) => {
     
     const parsedUrl = new URL(url)
     return parsedUrl.protocol
+}
+
+export const isFacebookGraphUrl = (url) => {
+    return url.includes('graph.facebook.com');
+};
+
+export const determineImageUrl = (url) => {
+    if (isFacebookGraphUrl(url)) {
+        return ''; 
+    } else if (defaultImagesUrl.includes(url)) {
+        return ''; 
+    }
+    return url; // Retorna la URL original en cualquier otro caso
 }
